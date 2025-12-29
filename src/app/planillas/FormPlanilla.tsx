c47bed2e6274f4d1a543cf32991790f4ebcacb1c
bryg97 <b.arroyaveg@gmail.com>
2025-12-29 10:14:28 -0500

    Fix: elimina fragmento JSX sobrante y asegura cierre correcto de return en FormPlanilla

--- src/app/planillas/FormPlanilla.tsx
+++ src/app/planillas/FormPlanilla.tsx
@@ -132,7 +132,7 @@ export default function FormPlanilla({
   }
 
   return (
-    <>
+    <div>
       {mostrarDetalleDeuda && deudaVehiculo && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-8 max-w-md w-full mx-4 max-h-[90vh] overflow-y-auto border-2 border-red-400">
@@ -466,8 +466,7 @@ export default function FormPlanilla({
         </button>
       </div>
     </form>
-  </div>
-  </div>
-  </>
+    </div>
+    </div>
   );
 }
